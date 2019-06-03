5e2721aed3abb015f85d101a31c58e0304578831
Chris Gonzalez <cmgonza89@gmail.com>
2019-06-02 21:55:22 -0500

    build out order component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
     render() {
         const{ image, name, price, desc, status } = this.props.details;
-
+        const isAvailable = status === "available";
 
         return (
             <li className="menu-fish">
@@ -12,7 +15,9 @@ class Fish extends React.Component {
                 <h3 className="fish-name">{name}</h3>
                 <span className="price">{formatPrice(price)}</span>
                 <p>{desc}</p>
-                <button>Add to Cart!</button>
+                <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+                    { isAvailable ? 'Add to Order' : 'Sold out!' }
+                </button>
 
             </li>
         );
